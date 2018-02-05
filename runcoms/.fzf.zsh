bb46b757334c86f4dc0161ea3e10d740260a9fe8
und3rdg <und3rdg@gmail.com>
2018-02-05 00:03:07 +0000

    fzf

---
+++ runcoms/.fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/undg/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/undg/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/undg/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/undg/.fzf/shell/key-bindings.zsh"
+
